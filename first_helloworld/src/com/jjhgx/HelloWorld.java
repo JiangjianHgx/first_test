b8dd6c062235e3a566e370ea60b81333a0ab1c4a
JiangjianHgx <980758897@qq.com>
2019-11-30 15:22:07 +0800

    add HelloWorld.java

---
+++ first_helloworld/src/com/jjhgx/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.jjhgx;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("hello world");
+        System.out.println(0%2);
+        System.out.println(0%3);
+    }
+}
