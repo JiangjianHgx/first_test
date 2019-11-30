9235de03a69dc7f35a0a8284fcceb2a49b159bcc
JiangjianHgx <980758897@qq.com>
2019-11-30 15:50:17 +0800

    add Test.java

---
+++ first_helloworld/src/com/jjhgx/Test.java
@@ -0,0 +1,10 @@
+package com.jjhgx;/*
+ * @author 江健
+ * @date   2019/11/30 0030 15:49
+ */
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("byebye");
+    }
+}
